edb02e08bb275482f301f86e7fc9584b733a05d8
Yaroslav Borys <borrline@gmail.com>
2021-11-11 12:48:34 +0200

    12

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://restcountries.eu/rest/v2';
+
+function fetchCountry(countryName) {
+  return fetch(`${BASE_URL}/name/${countryName}`).then(response => {
+    //console.log(response.json());
+    return response.json();
+  });
+}
+
+export default { fetchCountry };
